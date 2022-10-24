ad6ce0fe164fff1b3f7d93d2f924c0194e69a653
Vence Jumar Sasing <rjsasing@gmail.com>
2022-10-25 04:52:35 +0800

    tabs-experience

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 import './App.css';
 
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const reponse = await fetch(url)
+    const newJobs = await reponse.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+  const { company, dates, duties, title } = jobs[value]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="tab">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`tab-function ${index === value && 'active-tab'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        <article className="tab-content">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="tab-desc">
+                <FaAngleDoubleRight className="tab-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button type="button" className="moreInfo">
+        more info
+      </button>
+    </section>
+  )
 }
 
-export default App;
+export default App
